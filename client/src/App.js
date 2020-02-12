acfb2f46c4f255e98479322679c1883842691302
nicholasrobinharrison <nicholas.robin.harrison@gmail.com>
2020-02-12 15:48:58 -0500

    added a user context and provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route } from "react-router-dom";
+import { userContext } from "./context/userContext";
 
 import { theme } from "./themes/theme";
 import Signup from "./pages/Signup";
@@ -9,13 +10,27 @@ import Login from "./pages/Login";
 import "./App.css";
 
 function App() {
+  const [user, setUser] = useState({});
+
+  const logout = () => {
+    return; // placeholder
+  };
+
+  const value = {
+    user: user,
+    setUser: setUser,
+    logout: logout
+  };
+
   return (
-    <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Route path="/signup" component={Signup} />
-        <Route path="/login" component={Login} />
-      </BrowserRouter>
-    </MuiThemeProvider>
+    <userContext.Provider value={value}>
+      <MuiThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
+        </BrowserRouter>
+      </MuiThemeProvider>
+    </userContext.Provider>
   );
 }
 
