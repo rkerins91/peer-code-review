701955b76a2d70857ae431ad592dc653b1de2ea4
nicholasrobinharrison <nicholas.robin.harrison@gmail.com>
2020-02-19 18:17:26 -0500

    removeed duplicated removeToken method

--- client/src/functions/jwt.js
+++ client/src/functions/jwt.js
@@ -5,10 +5,6 @@ const getToken = () => {
   return token;
 };
 
-export const removeToken = () => {
-  localStorage.removeItem("peercode-auth-token");
-};
-
 const decodeToken = token => {
   var decodedToken = jwtDecode(token);
   return decodedToken;
