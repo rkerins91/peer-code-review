fabc1eefedcd2e2aebe81412bd8ba779248d6b7e
ashan <ashan36@gmail.com>
2020-02-25 17:45:53 -0800

    added login event

--- server/services/socketEvents.js
+++ server/services/socketEvents.js
@@ -3,7 +3,14 @@ const io = require("socket.io")();
 module.exports = {
   eventConfig: socket => {
     console.log("Client connected via sockets");
+
+    // Subscribe this socket connection to a room keyed by their userId
+    socket.on("login", userId => {
+      socket.join(userId);
+    });
   },
 
-  broadcast: (roomName, eventName, data) => {}
+  broadcast: (roomName, data) => {
+    io.to(roomName).emit("notification", data);
+  }
 };
